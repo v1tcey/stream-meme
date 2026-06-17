97feea2a59e90d4d0639cea2e555d8abee9905cd
v1tcey <vitalievaleksandr@gmail.com>
2026-06-17 20:18:52 +0200

    update: canvas, audio, animations

--- server.js
+++ server.js
@@ -2,15 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-
 app.use(express.static('public'));
-
 io.on('connection', (socket) => {
-    socket.on('meme', (data) => {
-        if (data.pass === process.env.APP_PASSWORD) {
-            io.emit('show', data);
-        }
+    socket.on('meme', (d) => {
+        if (d.pass === process.env.APP_PASSWORD) io.emit('show', d);
     });
 });
-
 http.listen(process.env.PORT || 3000);
